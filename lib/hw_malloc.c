ba29158ddb2a2707f68737a48df6749a23e37b06
F94036089 <f94036089@ncku.edu.tw>
2018-01-06 16:41:44 +0800

    Get started.

--- lib/hw_malloc.c
+++ lib/hw_malloc.c
@@ -8,14 +8,14 @@ void *hw_malloc(size_t bytes)
 
 	void* alloc_start_addr;
 	void* free_start_addr;
-	if (!first_alloc){
+	if (!first_alloc) {
 		heap_start_addr = sbrk(64 * 1024);
 		if(*(int*)heap_start_addr==-1)
 			return NULL;
 		alloc_start_addr = heap_start_addr + 40;
 		free_start_addr = alloc_start_addr + bytes;
 
-	}else{
+	} else {
 
 	}
 	return alloc_start_addr;
