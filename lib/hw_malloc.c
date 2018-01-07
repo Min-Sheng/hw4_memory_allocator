8bfca2337763bbb376333e6b0ab29b52d12a09ad
F94036089 <f94036089@ncku.edu.tw>
2018-01-07 13:43:11 +0800

    Build chunk and bin.

--- lib/hw_malloc.c
+++ lib/hw_malloc.c
@@ -1,21 +1,70 @@
 #include "hw_malloc.h"
 
-void* heap_start_addr;
-int first_alloc = 0;
+void bin_init(bin_t *bin_)
+{
+	bin_->next = (chunk_ptr_t)bin_;
+	bin_->prev=(chunk_ptr_t)bin_;
+}
 
+int bin_is_empty(bin_t *bin_)
+{
+	if(bin_->next==(chunk_ptr_t)bin)
+		return 1;
+	else
+		return 0;
+}
+
+void bin_add_chunk(bin_t *bin_,chunk_ptr_t chunk_ptr)
+{
+	chunk_ptr_t last=bin_->prev;
+	if(bin_==&bin[6]){
+		chunk_ptr_t tmp = bin_->next;
+		while (tmp != (chunk_ptr_t)&bin[6])
+		{
+			if(chunk_ptr->chunk_size > tmp->chunk_size){
+				last = tmp->prev;
+				bin_ =(bin_t*)tmp;
+				break;
+			}
+			tmp = tmp->next;
+		}
+	}
+	last->next = chunk_ptr;
+	bin_->prev = chunk_ptr;
+	chunk_ptr->next = (chunk_ptr_t)bin_;
+	chunk_ptr->prev = last;
+}
+
+void bin_show(bin_t *bin_){
+	chunk_ptr_t current = bin_->next;
+	while (current != (chunk_ptr_t)bin_)
+	{
+		printf("free chunk: %p\n", current);
+		current = current->next;
+	}
+}
 void *hw_malloc(size_t bytes)
 {
 
 	void* alloc_start_addr;
 	void* free_start_addr;
-	if (!first_alloc) {
-		heap_start_addr = sbrk(64 * 1024);
+	if (&bin[0]) {
+		heap_start_addr = sbrk(65536);
 		if(*(int*)heap_start_addr==-1)
 			return NULL;
 		alloc_start_addr = heap_start_addr + 40;
 		free_start_addr = alloc_start_addr + bytes;
-
-	} else {
+		chunk_ptr_t new_chunk_ptr = (chunk_ptr_t)free_start_addr;
+		new_chunk_ptr->chunk_size = 65536 - (40*2 + bytes);
+		new_chunk_ptr->pre_chunk_size = bytes;
+		new_chunk_ptr->prev_free_flag = 0;
+		int i = 0;
+		for (i = 0; i < 7;i++)
+			bin_init(&bin[i]);
+		bin_add_chunk(&bin[6],new_chunk_ptr);
+	}
+	else
+	{
 
 	}
 	return alloc_start_addr;
@@ -28,6 +77,7 @@ int hw_free(void *mem)
 
 void *get_start_sbrk(void)
 {
+	bin_show(&bin[6]);
 	brk(heap_start_addr);
 	return heap_start_addr;
 }
--- lib/hw_malloc.h
+++ lib/hw_malloc.h
@@ -6,9 +6,8 @@
 #include <unistd.h>
 #include <stdio.h>
 
-typedef void* chunk_ptr_t ;
-typedef unsigned long long chunk_size_t;
-typedef void* chunk_flag_t ;
+typedef struct chunk_header* chunk_ptr_t ;
+typedef unsigned long long chunk_size_t, chunk_flag_t ;
 
 struct chunk_header {
 	chunk_ptr_t prev;
@@ -17,6 +16,20 @@ struct chunk_header {
 	chunk_size_t pre_chunk_size;
 	chunk_flag_t prev_free_flag;
 };
+
+typedef struct {
+	chunk_ptr_t prev;
+	chunk_ptr_t next;
+}bin_t;
+
+bin_t bin[7];
+
+void* heap_start_addr;
+
+void bin_init(bin_t *bin_);
+int bin_is_empty(bin_t *bin_);
+void bin_add_chunk(bin_t *bin_, chunk_ptr_t chunk_ptr);
+void bin_show(bin_t *bin_);
 
 extern void *hw_malloc(size_t bytes);
 extern int hw_free(void *mem);
